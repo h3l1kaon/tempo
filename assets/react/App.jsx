bbe8c06dba42d6069fb97c388cf17e93218ba218
h3l1kaon <h3likaon@protonmail.com>
2022-10-06 16:42:49 +0200

    explore = home

--- assets/react/App.jsx
+++ assets/react/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import '../styles/app.scss'
-import AudioPack from './components/audio_pack/AudioPack'
-import Sidenav from './components/sidenav/Sidenav'
-import AlbumPage from './pages/AlbumPage'
-import ExplorePage from './pages/explore/ExplorePage'
-import HomePage from './pages/HomePage'
-import UserPlaylistsPage from './pages/UserPlaylistsPage'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import '../styles/app.scss';
+import AudioPack from './components/audio_pack/AudioPack';
+import Sidenav from './components/sidenav/Sidenav';
+import AlbumPage from './pages/AlbumPage';
+import HomePage from './pages/home/HomePage';
+import UserPlaylistsPage from './pages/UserPlaylistsPage';
 
 const App = () => {
 	return (
@@ -17,12 +16,12 @@ const App = () => {
 					<Route exact path="/" element={<HomePage />} />
 					<Route path="/album" element={<AlbumPage />} />
 					<Route path="/playlists" element={<UserPlaylistsPage />} />
-					<Route path="/explore" element={<ExplorePage />} />
+					<Route path="/explore" element={<HomePage />} />
 				</Routes>
 				<AudioPack />
 			</div>
 		</BrowserRouter>
-	)
-}
+	);
+};
 
-export default App
+export default App;
