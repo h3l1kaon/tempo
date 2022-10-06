089122d8d7e79ff75a8ccc0cbe6ba506b6086481
Maxime <uu@example.com>
2022-10-06 16:53:54 +0200

    Conflit résolu

--- assets/react/App.jsx
+++ assets/react/App.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '../styles/app.scss';
 import AudioPack from './components/audio_pack/AudioPack';
 import Sidenav from './components/sidenav/Sidenav';
-import AlbumPage from './pages/AlbumPage';
 import HomePage from './pages/home/HomePage';
+import TopNav from './components/top_nav/TopNav';
+import Modal from './components/modal/Modal';
+import AlbumPage from './pages/AlbumPage';
 import UserPlaylistsPage from './pages/UserPlaylistsPage';
 
 const App = () => {
+	const [openModal, setOpenModal] = useState(false);
+	useEffect(() => {
+		console.log(openModal);
+	}, [openModal]);
+
 	return (
 		<BrowserRouter>
 			<Sidenav />
 			<div className="container">
+				<TopNav triggerModal={setOpenModal} />
+				<Modal openned={openModal} close={setOpenModal} />
+
 				<Routes>
 					<Route exact path="/" element={<HomePage />} />
 					<Route path="/album" element={<AlbumPage />} />
