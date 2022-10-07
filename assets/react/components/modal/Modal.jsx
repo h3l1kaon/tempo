5f12d1f9f190e5535939176a9b4e9c0e6a8a289c
h3l1kaon <h3likaon@protonmail.com>
2022-10-07 09:54:52 +0200

    modif

--- assets/react/components/modal/Modal.jsx
+++ assets/react/components/modal/Modal.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './modal.scss';
 
 function Modal({ openned, close }) {
+	console.log(openned);
 	return (
 		<div
 			className="modal"
 			style={{
-				display: openned ? 'flex' : 'none',
+				display: openned.open ? 'flex' : 'none',
 			}}
 		>
 			<div className="content">
+				<h1>{openned.val}</h1>
 				<span onClick={() => close(false)}>X</span>
 				<h1>Test Formulaire modal</h1>
 				<p>olololeazeaeaeaea</p>
